eeea3d15e0fef962b532446c298571ed5852037e
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-24 12:52:28 -0700

    Added header files for Customer

---
+++ src/customer.cpp
@@ -0,0 +1,12 @@
+// -------------------------------- customer.h ---------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class implements the creation of a customer object. Contains 
+// information about the customer as well as the movies they have checked out
+// and their transaction history
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - Input provided to the constructor will be given in the form of: 
+//      customerID# (int) lastName (string) firstName (string)
+
+#include "customer.h"
