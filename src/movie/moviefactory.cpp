da589ac208b7e9b5d69bb38e85fb029e6cf02013
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-24 13:00:34 -0700

    Added header files for MovieFactory

---
+++ src/movie/moviefactory.cpp
@@ -0,0 +1,12 @@
+// ----------------------------- moviefactory.cpp ------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class defines a factory that is responsible for determining
+// what type of Movie object should be instantiated based on provided
+// input. Allows for dynamic binding of Movie objects.
+// ------------------------------------------------------------------------
+// Assumptions:
+//  - File input will be provided in the formats neccessary for each type of 
+//    associated Movie genre
+
+#include "moviefactory.h"
