9b72e614418d89800ef1ac6a7f7f247fa058635c
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-24 13:03:25 -0700

    Added header files for Classics

---
+++ src/movie/classics.cpp
@@ -0,0 +1,12 @@
+// --------------------------------- classics.cpp ------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class defines a classic movie. It will contain all information 
+// related to the Movie and allow for comparisons to other Classics objects.
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - All input data will be provided in the following format:
+//     genre(string) stock(int) director(string) title(string) releaseMonth(int)
+//     releaseYear(int) 
+
+#include "classics.h"
---
+++ src/movie/classics.h
@@ -0,0 +1,10 @@
+// -------------------------------- classics.h ---------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class defines a classic movie. It will contain all information 
+// related to the Movie and allow for comparisons to other Classics objects.
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - All input data will be provided in the following format:
+//     genre(string) stock(int) director(string) title(string) releaseMonth(int)
+//     releaseYear(int) 
