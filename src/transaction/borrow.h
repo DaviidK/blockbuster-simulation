482edf8c0c0911523bda521d817dc9cd2b03956f
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-25 11:13:52 -0700

    Added header files for Borrow

---
+++ src/transaction/borrow.h
@@ -0,0 +1,10 @@
+// --------------------------------- borrow.h ----------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class defines the action of a Customer borrowing a Movie from a
+// Store. Will contain information related to the specific Movie being borrowed 
+// as well as the Customer that is borrowing the movie 
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - File input will be provided in the following format:
+//      customerID#(int) mediaFormat(char) movieData(formatted based on genre)
