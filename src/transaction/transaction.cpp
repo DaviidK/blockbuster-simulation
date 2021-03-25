97f003cd9f0027a4e3631a7a9b230e5e9bdb6455
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-25 11:11:10 -0700

    Added header files for Transaction

---
+++ src/transaction/transaction.cpp
@@ -0,0 +1,13 @@
+// ----------------------------- transaction.cpp -------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class implements an interface for a single transaction. The
+// object created from this class may be related to Movie, Store, or Customer 
+// objects depending on which type of Transaction object is instantiated.
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - All input data will be provided in the format required by the associated 
+//    Transaction class
+
+
+#include "transaction.h"
