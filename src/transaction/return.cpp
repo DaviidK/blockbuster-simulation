da2216d4f45b5ffbfff139f61144ca48f4121700
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-25 11:14:49 -0700

    Added header files for Return

---
+++ src/transaction/return.cpp
@@ -0,0 +1,13 @@
+// ------------------------------- return.cpp ----------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class implements an object that will conduct the action of a
+// Customer returning a Movie to a Store. Will contain information related to
+// the specific Movie being returned as well as the Customer that is returning 
+// the movie 
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - All input data will be provided in the following format:
+//       actionType(char) customerID#(int) mediaFormat(char) movieData
+
+#include "return.h"
