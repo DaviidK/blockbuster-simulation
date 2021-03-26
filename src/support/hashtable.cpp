b9530ceb76fe25ce489d3397982cd2f19857053e
unknown <49457408+DaviidK@users.noreply.github.com>
2021-03-26 12:25:50 -0700

    Added header files for Hashtable

---
+++ src/support/hashtable.cpp
@@ -0,0 +1,9 @@
+// ------------------------------- hashtable.cpp -------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class will define a HashTable that stores data in an array. The
+// HashTable will use open hashing to compute the correct array index in which
+// to store the data.
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - This Hashtable stores a given key-value pairs, where key is always an int.
---
+++ src/support/hashtable.h
@@ -0,0 +1,9 @@
+// ------------------------------- hashtable.h ---------------------------------
+// David Kang
+// -----------------------------------------------------------------------------
+// Purpose: This class will define a HashTable that stores data in an array. The
+// HashTable will use open hashing to compute the correct array index in which
+// to store the data.
+// -----------------------------------------------------------------------------
+// Assumptions:
+//  - Only ints will be accepted as Key values for this table
