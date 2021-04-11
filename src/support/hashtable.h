9ab92b9338824a7f0c80af5cd91580f6f3f10f4d
unknown <49457408+DaviidK@users.noreply.github.com>
2021-04-10 18:48:18 -0700

    Outlined hashtable header and instantiation files

--- src/support/hashtable.h
+++ src/support/hashtable.h
@@ -7,3 +7,35 @@
 // -----------------------------------------------------------------------------
 // Assumptions:
 //  - Only ints will be accepted as Key values for this table
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+using namespace std;
+
+template<class Value> 
+class HashTable {
+
+    public:
+        // Constructor: Will create an empty HashTable object
+        HashTable();
+        // Destructor: Will delete the HashTable object
+        ~HashTable();
+        // Inserts a new object at the passed Key location
+        void insert(const int&, const Value&);
+        // Removes the object contained in the passed Key location
+        void remove(const int&);
+        // Retrieves the object present at the passed Key location
+        Value* retrieve(const int&) const;
+
+    private: 
+        // Array that holds the data
+        Value* data;
+        // Array size
+        int size;
+        // Hashing function: Takes in a Key and returns the hashed index
+        int hash(const int&) const;
+
+};
+
+#endif
