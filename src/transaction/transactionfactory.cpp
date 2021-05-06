6836331ba3d87314c9715bc8b2326cdecba286f2
David Kang <49457408+DaviidK@users.noreply.github.com>
2021-05-05 19:51:42 -0700

    Finished first draft of TransactionFactory

--- src/transaction/transactionfactory.cpp
+++ src/transaction/transactionfactory.cpp
@@ -10,4 +10,53 @@
 //  - File input will be provided in the formats neccessary for each type of 
 //    associated Transaction object
 
-#include "transactionfactory.h"
+#include "transactionFactory.h"
+
+//----------------------------------------------------------Public member method
+// Will parse through passed parameter information to determine which type of
+// Transaction object to create and return
+Transaction createTransaction(string input,
+                              map<string, BinTree<Movie>>& inventory,
+                              HashTable<int, Customer>& customers) const {
+    // Place data from passed input into stringstream
+    stringstream inputStream; 
+    inputStream << input;
+
+    // Pull the first character of the passed string to determine the type of
+    // Transaction object to be created
+    char transType;
+    inputStream >> transType;
+
+    // Create a Transaction object and instantiate it according to the character
+    // found in input. For each type of Transaction, return NULL if object
+    // instantiated was invalid
+    Transaction t;
+    swtich (transType):
+        case("B"):
+            t = Borrow(inputStream, inventory, customers);
+            if (t.movie == NULL || t.customer == NULL) {
+                return NULL;
+            }
+            break;
+        case("H"):
+            t = History(inputStream, customers);
+            if (t.customer == NULL) {
+                return NULL;
+            }
+            break;
+        case("I"): 
+            t = Inventory(inventory);
+            break;
+        case("R"):
+            t = Return(inputStream, inventory, customers);
+            if (t.movie == NULL || t.customer == NULL) {
+                return NULL;
+            }
+            break;
+        case(default):
+            cout << "ERROR: Transaction type \"" << transType 
+            << "\" is not valid";
+            return NULL;
+    
+    return t;
+}
--- src/transaction/transactionfactory.h
+++ src/transaction/transactionfactory.h
@@ -8,3 +8,23 @@
 // Assumptions:
 //  - File input will be provided in the formats neccessary for each type of 
 //    associated Transaction object
+
+#ifndef TRANSACTIONFACTORY_H
+#define TRANSACTIONFACTORY_H
+
+#include "transaction.h"
+
+using namespace std;
+
+class TransactionFactory {
+    public:
+        //-------------------------------------------------Public member methods
+        // Will parse through passed parameter information to determine which
+        // type of Transaction object to create and return
+        Transaction createTransaction(string info,
+                                      HashTable<string, Movie>&,
+                                      HashTable<int, Customer>&) const;
+                                      
+};
+
+#endif
