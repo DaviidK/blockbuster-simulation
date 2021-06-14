28bbabf24205f50119713a64c79bb353005d08b8
David Kang <49457408+DaviidK@users.noreply.github.com>
2021-06-13 21:24:02 -0700

    First draft implementation of movie

--- src/movie/movie.cpp
+++ src/movie/movie.cpp
@@ -16,3 +16,137 @@
 //         Classics: majorActor(string) releaseMonth(int) releaseYear(int)
 
 #include "movie.h"
+#include <sstream>
+#include <algorithm>
+
+//---------------------------------------------------Constructors and Destructor
+/**
+ * Default Constructor
+ * Creates a Movie object
+ */
+Movie::Movie() {}
+
+//---------------------------------------------------------Public Data Accessors
+/**
+ * public getGenre
+ * Returns the movie genre
+ */
+char Movie::getGenre() const {
+    return this->genre;
+}
+
+/**
+ * public getDirector
+ * Returns the movie director
+ */
+string Movie::getDirector() const {
+    return this->director;
+}
+
+/**
+ * public getDirector
+ * Returns the movie director
+ */
+string Movie::getTitle() const {
+    return this->title;
+}
+
+
+/**
+ * public getReleaseYear
+ * Returns the movie release year
+ */
+int Movie::getReleaseYear() const {
+    return this->releaseYear;
+}
+
+/**
+ * public getStockInStore
+ * Returns how many copies of the movie are available in the store
+ */
+int Movie::getStockInStore() const {
+    return this->stockInStore;
+}
+
+//---------------------------------------------------------Public Data Modifiers
+/**
+ * public increaseStock
+ * Increases the stock by 1
+ * @return bool true if stock is increased, else false
+ */
+bool Movie::increaseStock() {
+    this->stockInStore++;
+    return true;
+}
+
+/**
+ * public decreaseStock
+ * Decreases the stock by 1
+ * @return bool true if stock is decreased, else false
+ */
+bool Movie::decreaseStock() {
+    // check if there is stock left
+    if (this->stockInStore > 0) {
+        this->stockInStore--;
+        return true;
+    } else {
+        // if stock is not available
+        return false;                                       // TODO should we print a massage here?
+    }
+}
+
+//------------------------------------------------------Protected Helper Methods
+/**
+ * protected checkValidInput
+ * Check for invalid input. If input is invalid, prints an error and sets the 
+ * Movie title to "".
+ * @param[in] stringstream&: Data from input file
+ * @return bool if valid
+ */
+bool Movie::checkValidInput(stringstream& input, const string& field) {
+    string dataString;
+    int dataInt;
+
+    if (field == "Stock" || field == "Year") {
+        input >> dataInt;
+        
+        // Check for valid integer input
+        if (input.fail()) {
+            cout << "ERROR: Movie " << field <<" is not an number" << endl;
+            this->title = "";
+            return false;
+        }
+        
+        // Check for valid input
+        if (input.peek() == ',') {
+            input.ignore();
+        }
+        
+        // Save valid data input
+        if (field == "Stock") this->stockInStore = dataInt;
+        else this->releaseYear = dataInt;
+        return true;
+
+    } else if (field == "Director" || field == "Title") {
+        getline(input, dataString, ',');
+        
+        // Trim leading spaces and commas
+        remove(dataString.begin(), dataString.end(), ' ');
+        
+        // Check for valid input
+        if (dataString == "") {
+            cout << "ERROR: Empty " << field << " is not valid" << endl;
+            this->title = "";
+            return false;
+        }
+        
+        // Save valid data input
+        if (field == "Director") this->director = dataString;
+        else this->title = dataString;
+        return true;
+
+    }
+    
+    // invalid field
+    return false;
+}
