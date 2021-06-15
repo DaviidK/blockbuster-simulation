f5192c89190daa2a7d424ff2ff7c13ea699a08b9
David Kang <49457408+DaviidK@users.noreply.github.com>
2021-06-14 23:51:36 -0700

    First draft instantiation of store

--- src/store/store.cpp
+++ src/store/store.cpp
@@ -24,3 +24,100 @@
 
 
 #include "store.h"
+
+//---------------------------------------------------Constructors and Destructor
+/**
+ * Constructor: Assigns the passed parameter to the storeName field and 
+   instantiates all other fields to null
+ * @param[string]: String containing the name of the store
+*/ 
+Store::Store(string name) { 
+    this->storeName = name;
+} 
+
+Store::~Store() { 
+  // delete this->customers;
+  // delete this->inventory;
+  // delete this->transactions;
+} 
+
+/**
+ * populateInventory: Populates the inventory field by parsing through the 
+   passed data and creating Movie objects for each line of data
+ * @param[ifstream&]: Input file containing data for all movies that the store
+   will have in inventory
+*/
+void Store::populateInventory(ifstream& input) {
+    this->inventory = map<char, BinTree>();
+    MovieFactory movieFac = MovieFactory(); 
+    string line;
+    // Read input until file is empty
+    while (!input.eof()) {
+        // Pull one line, representing a single movie, from the input file
+        getline(input, line);
+        // Construct a Movie object using the Movie Factory
+        Movie* m = movieFac.createMovie(line);
+        char genre = m->getGenre();
+        // If the genre does not yet exist in the map, create a new BSTree
+        // and link it to the genre
+        if (m != NULL && this->inventory.find(genre) != this->inventory.end()) {
+            BinTree movieTree = BinTree();
+            // Add the created movie into the BSTree
+            movieTree.insert(m);
+            // Add the BSTree into the map
+            this->inventory.insert({genre, movieTree});
+        } else if (m != NULL) {
+            // If the genre does exist in the map, add the movie into the 
+            // corresponding tree
+            this->inventory.at(genre).insert(m);
+        }   
+    }
+}
+
+
+/**
+ * populateCustomers: Populates the customers field by parsing through the 
+   passed data and creating Customer objects for each line of data
+ * @param[ifstream&]: Input file containing data for all customers that 
+   will be interacting with the Store
+*/
+void Store::populateCustomers(ifstream& input) {
+    // Assuming a maximum of 500 customers, instantiate the HashTable with a
+    // size of 503, the next prime number after 500.
+    this->customers = HashTable<Customer>(503);
+    string line;
+    // Read input until file is empty
+    while (!input.eof()) {
+        // Pull one line, representing a single movie, from the input file
+        getline(input, line);
+        // Construct a Customer object by passing the input line
+        const Customer* c = new Customer(line); 
+        // Add the customer into the HashTable
+        this->customers.insert(c->getCustomerID(), c); 
+    }
+}
+
+
+/**
+ * readTransactions: Populates the transactions field by parsing through the 
+   passed data and creating Transaction objects for each line of data
+ * @param[ifstream&]: Input file containing data for all transactions that 
+   the store will be executing
+*/
+void Store::readTransactions(ifstream& input) {
+    TransactionFactory transFac = TransactionFactory();
+    string line;
+    // Read input until file is empty
+    while (!input.eof()) {
+        // Pull one line, representing a single movie, from the input file
+        getline(input, line);
+        // Construct a Transaction object using the transaction factory
+        Transaction* t = transFac.createTransaction(line, this->inventory,
+                                                    this->customers); 
+        // Only insert the Transaction object into the transactions field if
+        // a valid input was provided 
+        if (t != NULL) {
+            this->transactions.push_back(t); 
+        }
+    }
+}
