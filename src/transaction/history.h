6065f90730fa3d0d830dad5ad4f789b6fdd1b2c3
David Kang <49457408+DaviidK@users.noreply.github.com>
2022-01-11 16:44:56 -0800

    Finished instantiating History

--- src/transaction/history.h
+++ src/transaction/history.h
@@ -8,32 +8,35 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 
+#include <sstream>
+#include <iomanip>
 #include "transaction.h"
 #include "borrow.h"
 #include "return.h"
 #include "../customer/customer.h"
 #include "../support/hashtable.h"
-#include <sstream>
 
 using namespace std;
 
 class History : public Transaction {
+    
     public:
         //-------------------------------------------------Public member methods
         // Constructor: Will create a History object and assign a Customer
         // object to the c field
-        History(stringstream&, HashTable<Customer>&);
+        History(string&, HashTable<Customer>&);
         // Destructor: Will destroy the History object
         virtual ~History();
         // Prints the Customer's transaction history in chronological order
         virtual void doTransaction() const;
-        
+        // Returns the Customer
         Customer* getCustomer() const;
 
     private:
         //-------------------------------------------------Private member fields
         // Pointer to the Customer object associated with this transaction
         Customer* customer;
+
 };
 
 #endif
