c264c9a587070271dd2cab2e7d56314deb3d39b8
David Kang <49457408+DaviidK@users.noreply.github.com>
2022-01-12 10:08:35 -0800

    Finished implementing Classics

--- src/movie/classics.cpp
+++ src/movie/classics.cpp
@@ -10,57 +10,108 @@
 //     releaseYear(int) 
 
 #include "classics.h"
-#include <sstream>
-#include <algorithm>
+
+/**
+ * printElement
+ * Helper method that allows for easy printing of elements at set intervals
+ * @param[in] Typename T: Data to be printed
+ * @param[in] int width: Width between next element on print line
+ * @return string: Resulting string after element adjustment
+ */
+template<typename T> string printElement(T t, const int& width) {
+    const char separator = ' ';
+    stringstream result; 
+    result << left << setw(width) << setfill(separator) << t;
+    return result.str();
+}
+
+/**
+ * Overloads the ostream << operator
+ * Prints the Movie details and stock
+ * @param[in] ostream output;
+ * @param[in] Classics movie: details to displayed
+ * @return ostream : output representing a Classics object
+ */
+ostream& operator<<(ostream& output, const Classics& movie){
+    output << printElement(movie.getTitle(), 25);
+    output << printElement(movie.getDirector(), 24);
+    output << printElement(movie.getReleaseMonth(), 13);
+    output << printElement(movie.getReleaseYear(), 10);
+    
+    // Loop through the major actors array, combining all actors into a stream
+    int arraySize = movie.getAllMajorActors().size();
+    stringstream allActors;
+    for (int i = 0; i < arraySize - 1; i++) {
+        allActors << movie.getAllMajorActors()[i] << ", ";
+    }
+    allActors << movie.getAllMajorActors()[arraySize - 1];
+    // Output the resulting string after all major actors have been concatenated
+    output << printElement(allActors.str(), 35);
+    output << printElement(movie.getFormat(), 8);
+    output << printElement(movie.getStockInStore(), 0);
+    output << endl;
+    // return ostream&
+    return output;
+}
 
 //---------------------------------------------------Constructors and Destructor
 /**
- * Default Constructor
+ * Constructor
  * Creates a Movie object and populates the genre, director
- *  title, and releaseYear fields
- * If there's error on the input, Title is set to "". 
- * @param[in] string input: containing all fields
+ * title, release date and major actor fields
+ * If there's error on the input, Title is set to invalid (-1) 
+ * @param[in] string input : containing all fields
  */
-Classics::Classics(string input) {
+Classics::Classics(string input) : Movie() {
+    // Populate empty member variables
+    this->releaseMonth = -1;
+
+    // Set genre to Classics (C)
     this->genre = 'C';
     
     // Place data from passed input into stringstream
     stringstream inputStream(input);
     string dataString;
-    string dataInt;
 
     // Get stock info from input
-    if (!this->checkValidInput(inputStream, "Stock")) {
+    getline(inputStream, dataString, ',');
+    if (!this->checkValidInput(dataString, "Stock")) {
         return;
     }
 
     // Get director from input
-    if (!this->checkValidInput(inputStream, "Director")) {
+    getline(inputStream, dataString, ',');
+    if (!this->checkValidInput(dataString, "Director")) {
         return;
     }
 
     // Get title from input
-    if (!this->checkValidInput(inputStream, "Title")) {
+    getline(inputStream, dataString, ',');
+    if (!this->checkValidInput(dataString, "Title")) {
         return;
     }
 
     // Gets first name of Major Actor from Input
-    inputStream >> this->majorActor;
+    string firstName;
+    inputStream >> firstName;
     // Check for valid integer input
-    if (this->majorActor == "") {
+    if (firstName == "") {
         cout << "ERROR: No Major Actor Given" << endl;
         this->title = "";
         return;
     }
     // Gets second name of Major Actor from Input
-    inputStream >> dataString;
+    string secondName;
+    inputStream >> secondName;
     // Check for valid integer input
-    if (this->majorActor == "") {
+    if (secondName == "") {
         cout << "ERROR: No second name for Major Actor Given" << endl;
         this->title = "";
         return;
     }
-    this->majorActor = this->majorActor + " " + dataString;
+    // Combine Firstname and Last name of Major Actor
+    string actor = firstName + " " + secondName;
+    this->majorActors.push_back(actor);
 
 
     // Get Release month from input
@@ -80,75 +131,79 @@ Classics::Classics(string input) {
     }
 }
 
-Classics::Classics(char genre, string input) {
+/**
+ * Constructor
+ * Creates a Movie object and populates the genre, director
+ * title, release date and major actor fields
+ * If there's error on the input, Title is set to invalid (-1)
+ * @param[in] char genre : movie genre
+ * @param[in] string input : containing all fields after genre
+ */
+Classics::Classics(char genre, string input) : Movie() {
+    // Set genre to Classics (C)
     this->genre = 'C';
+    
+    // Populate empty member variables
+    this->releaseMonth = -1;
+
     // Place data from passed input into stringstream
     stringstream inputStream(input);
-    string dataString;
 
     // Get Release month from input
     inputStream >> this->releaseMonth;
     // Check for valid integer input
     if (inputStream.fail()) {
-        cout << "ERROR: Movie Release date is not an number" << endl;
+        cout << "ERROR: Movie release date is not an number" << endl;
         this->title = "";
         return;
     }
     inputStream >> this->releaseYear;
     // Check for valid integer input
     if (inputStream.fail()) {
-        cout << "ERROR: Movie Release date is not an number" << endl;
+        cout << "ERROR: Movie release date is not an number" << endl;
         this->title = "";
         return;
     }
 
     // Gets first name of Major Actor from Input
-    inputStream >> this->majorActor;
+    string firstName;
+    inputStream >> firstName;
     // Check for valid integer input
-    if (this->majorActor == "") {
+    if (firstName == "") {
         cout << "ERROR: No Major Actor Given" << endl;
         this->title = "";
         return;
     }
     // Gets second name of Major Actor from Input
-    inputStream >> dataString;
+    string secondName;
+    inputStream >> secondName;
     // Check for valid integer input
-    if (this->majorActor == "") {
+    if (secondName == "") {
         cout << "ERROR: No second name for Major Actor Given" << endl;
         this->title = "";
         return;
     }
-    this->majorActor = this->majorActor + " " + dataString;
-    this->title = "x";
+    // Combine Firstname and Last name of Major Actor
+    string actor = firstName + " " + secondName;
+    this->majorActors.push_back(actor);
 
+    this->title = "-1";
 }
 
-//---------------------------------------------------------Public Data Accessors
 /**
- * Overloads the ostream << operator
- * Prints the Movie details and stock
- * Output example: 
- * "TODO"
- * @param[out] ostream output;
- * @param[in] Classics movie: details to displayed
+ * virtual Destructor
+ * Destroys the Classic object
  */
-ostream& operator<<(ostream& output, const Classics& movie){
-    output << "Stock: " << movie.stockInStore
-           << "; Title: " << movie.title
-           << "; Director: " << movie.director
-           << "; Major Actor: " << movie.majorActor
-           << "; Release Date: " << movie.releaseMonth 
-           << " " << movie.releaseYear << endl;
-    // return ostream&
-    return output;
-}
+Classics::~Classics() {}
 
+//---------------------------------------------------------Public member methods
 /**
  * public virtual compareTo
- * Will compare this Classics object to the passed parameter Classics object
+ * Will compare this Classics object to the passed parameter Movie object
  * Returns 1 if this is greater than the parameter, -1 if this is less
  * than the parameter, and 0 if the two Movies are equal.
- * @param[in] Classics: Movie object to be compared with this object
+ * @param[in] Movie : Movie object to be compared with this object
+ * @return int : -1 (<), 0 (==), or 1 (>)
  */
 int Classics::compareTo(const Movie& movie) const {
     if (*this == movie) {
@@ -160,19 +215,22 @@ int Classics::compareTo(const Movie& movie) const {
     }
 }
 
-//---------------------------------------------------------Overloaded Operations
 /**
  * Overloads the == comparison operator
- * Compares each field for Classics given in parameter
- * @param[in] Classics movie: left hand side of operation
- * @return boolean: == comparison result
+ * Compares Release Date then Major actor (first stored) of this 
+ * to Movie given in rhs
+ * @param[in] Movie movie : right hand side of operation
+ * @return boolean : == comparison result
  */
 bool Classics::operator==(const Movie& movie) const {
     const Classics & m = static_cast<const Classics &>(movie);
     if (this->releaseMonth == m.getReleaseMonth() &&
-        this->releaseYear == m.getReleaseYear() &&
-        this->majorActor.compare(m.getMajorActor())) {
-            return true;
+        this->releaseYear == m.getReleaseYear()) {
+            if (movie.getTitle() == "-1") {
+                return (this->includesActor(m.getMajorActor()));
+            } else {
+                return (this->title.compare(m.getTitle()) == 0);
+            }
     }
     // Movie is not same
     return false;
@@ -180,9 +238,10 @@ bool Classics::operator==(const Movie& movie) const {
 
 /**
  * Overloads the != comparison operator
- * Compares each field for Classics given in parameter
- * @param[in] Classics movie: left hand side of operation
- * @return boolean: != comparison result
+ * Compares Release Date then Major actor (first stored) of this 
+ * to Movie given in rhs 
+ * @param[in] Movie movie : right hand side of operation
+ * @return boolean : != comparison result
  */
 bool Classics::operator!=(const Movie& movie) const {
     return !(*this == movie);
@@ -190,9 +249,10 @@ bool Classics::operator!=(const Movie& movie) const {
 
 /**
  * Overloads the > comparison operator
- * Compares Director then Title of this to Classics given in rhs
- * @param[in] Classics movie: left hand side of operation
- * @return boolean: > comparison result
+ * Compares Release Date then Major actor (first stored) of this 
+ * to Movie given in rhs
+ * @param[in] Movie movie : right hand side of operation
+ * @return boolean : > comparison result
  */
 bool Classics::operator>(const Movie& movie) const {
     const Classics & m = static_cast<const Classics &>(movie);
@@ -205,7 +265,7 @@ bool Classics::operator>(const Movie& movie) const {
             return true;
         } else if (this->releaseMonth == m.getReleaseMonth()) {
             // If lhs year&month == rhs year&month, check for major actor.
-            if (this->majorActor.compare(m.getMajorActor()) > 0) {
+            if (this->getMajorActor().compare(m.getMajorActor()) > 0) {
                 return true;
             }
         }
@@ -216,28 +276,62 @@ bool Classics::operator>(const Movie& movie) const {
 
 /**
  * Overloads the < comparison operator
- * Compares each field for Classics given in parameter
- * @param[in] Classics movie: left hand side of operation
- * @return boolean: < comparison result
+ * Compares Release Date then Major actor (first stored) of this 
+ * to Movie given in rhs
+ * @param[in] Movie movie : right hand side of operation
+ * @return boolean : < comparison result
  */
 bool Classics::operator<(const Movie& movie) const {
     const Classics & m = static_cast<const Classics &>(movie);
     return !(*this > m) && *this != m;
 }
 
-//---------------------------------------------------------Public Data Accessors
 /**
- * public getDirector
- * Returns the movie director
+ * public getMajorActor
+ * Returns the first Major Actor
+ * @return string : major actors
  */
 string Classics::getMajorActor() const {
-    return this->getMajorActor();
+    return this->majorActors[0];
 }
 
+/**
+ * public getAllMajorActors
+ * Returns all the Major Actor(s)
+ * @return vector<string> : major actors(s)
+ */
+vector<string> Classics::getAllMajorActors() const {
+    return this->majorActors;
+}
+
+/**
+ * public includesActor
+ * Returns true if the passed parameter is included in the majorActors field
+ * @param string actor : movie actor to be checked
+ * @return bool : true if actor is found in the array, else false
+ */
+bool Classics::includesActor(string s) const {
+    for (int i = 0; i < this->majorActors.size(); i++) {
+        if (this->majorActors[i] == s) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * public addActor
+ * Adds a new actor to the majorActors field
+ * @param string actor : movie actor to be added
+ */
+void Classics::addActor(string s) {
+    this->majorActors.push_back(s);
+}
 
 /**
  * public getReleaseYear
  * Returns the movie release month
+ * @return int : release month
  */
 int Classics::getReleaseMonth() const {
     return this->releaseMonth;
--- src/movie/classics.h
+++ src/movie/classics.h
@@ -12,6 +12,9 @@
 #ifndef CLASSICS_H
 #define CLASSICS_H
 
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 #include "movie.h"
 
 using namespace std;
@@ -21,14 +24,15 @@ class Classics : public Movie {
         friend ostream& operator<<(ostream&, const Classics&);
         
     public:
-        //-------------------------------------------------Public member methods
+        //-------------------------------------------Constructors and Destructor
         // Constructor: Creates a Classics object and populates the genre,
         // title, director, and releaseYear fields
         Classics(string);
         Classics(char, string);
         // Virtual destructor: Destroys the Classics object
         virtual ~Classics();
-        // Compares this object to other Classics objects
+        //-------------------------------------------------Public member methods
+        // Compares this object to other Movie objects
         virtual int compareTo(const Movie &) const;
         // Overloads the == comparison operator
 		virtual bool operator==(const Movie &) const;
@@ -38,17 +42,25 @@ class Classics : public Movie {
         virtual bool operator>(const Movie &) const;
         // Overloads the < comparison operator
 		virtual bool operator<(const Movie &) const;
-        // Returns the major actor
+        // Returns the first major actor listed
         string getMajorActor() const;
+        // Returns the vector of all major actors associated with a movie
+        vector<string> getAllMajorActors() const;
+        // Returns true if the passed parameter is included in the majorActors
+        // field
+        bool includesActor(string) const;
+        // Adds a new actor to the majorActors field
+        void addActor(string);
         // Returns the movie release month
         int getReleaseMonth() const;
 
     private:
         //-------------------------------------------------Private member fields
         // Main actor in the movie
-        string majorActor;
+        vector<string> majorActors;
         // Month that the movie was released in
         int releaseMonth;
+        
 };
 
 #endif
